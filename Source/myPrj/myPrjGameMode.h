f8c1451ccca40db2f7e58b078bed29cccdaa9cc5
1900Lemon <liuzheming113@163.com>
2024-11-19 19:07:39 +0800

    WEEK1

---
+++ Source/myPrj/myPrjGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "myPrjGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AmyPrjGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AmyPrjGameMode();
+};
+
+
+
