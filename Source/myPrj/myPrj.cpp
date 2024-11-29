1365cf2850897746036c1e3150c37a36356b969a
1900Lemon <liuzheming113@163.com>
2024-11-29 20:18:59 +0800

    Delete Source directory

--- Source/myPrj/myPrj.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "myPrj.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, myPrj, "myPrj" );
- 
